446ab603964aebe70fb0bef27e24dcd6c25a7912
MiroslavSapunar <psapunar@fi.uba.ar>
2022-11-24 23:26:44 -0300

    refactor

--- script.py
+++ script.py
@@ -1,57 +1,61 @@
 import csv
+import datetime
+import random
 
-# materia = {
-#     "codigo": "XXXX",
-#     "nombre": "Nn",
-#     "Fechas":[],
-# }
+class Materia:
+    def __init__(self, codigo=0, nombre=0):
+        self.codigo = codigo
+        self.nombre = nombre
+        self.examenes = []
 
+    def agregar_fecha(self, fecha):
+        self.examenes.append(Examen(fecha, random.randrange(15, 150)))
 
-def info_materia(materia):
-    print(f'codigo: {materia["codigo"]}, nombre: {materia["nombre"]}')
+    def obtener_examenes(self):
+        return self.examenes
 
-def info_fechas_materia(materia):
-    i = 1
-    for date in materia["fechas"]:
-        print(f'fecha nº{i} el {date}')
-        i+=1
+    def optener_codigo(self):
+        return self.codigo
+
+    def checkear_codigo(self, codigo):
+        return self.codigo == codigo
+    
+    def imprimir_info(self):
+        print(f'codigo: {self.codigo}, nombre: {self.nombre}')
+        for examen in self.examenes:
+            examen.imprimir_info()
+
+class Examen:
+    def __init__(self, fecha, inscriptos = 0):
+        self.fecha = fecha
+        self.inscriptos = inscriptos
+
+    def imprimir_info(self):
+        print(f'fecha: {self.fecha}, incriptos: {self.inscriptos}')
+
+        
 
 
 materias = []
 
 with open('./Data/Dic 2022 - Feb-Mar 2023-Table 1.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=';')
-    header = next(csv_reader)
-    print(header)
+    # header = next(csv_reader)
+    next(csv_reader)
+    # print(header)
     # line_count = 0
     for row in csv_reader:
-        if not any(materia['codigo'] == row[0] for materia in materias):
+        if not any(materia.checkear_codigo(row[0]) for materia in materias):
             materias.append(
-                {
-                    "codigo": row[0],
-                    "nombre":row[1],
-                    "fechas": [row[5]]
-                }
+                Materia(row[0], row[1])
             )
-        else:
-            materia = materias[-1]
-            print('materia: ', str(materia))
-            # (materias[index])["fechas"].append(row[6])
-            # materias['fechas'] + row[6]
+        materias[-1].agregar_fecha( datetime.datetime.strptime(row[5]+ "-" + row[6], '%m/%d/%y-%H:%M:%S %p'))
 
+
+    for materia in materias:
+        materia.imprimir_info()
 
     print("cantidad de materias cargadas: ",len(materias))
-    # for materia in materias:
-    #     info_materia(materia)
-    #     info_fechas_materia(materia)
-    #     if line_count == 0:
-    #         print(f'Las columnas de archivo son: {" - ".join(row)}')
-    #         line_count += 1
-    #     else:
-    #         print(f'Sede: {row[0]}, piso: {row[1]}, aula nº: {row[2]}.')
-    #         line_count += 1
-    # print(f'Processed {line_count} lines.')
-
 
 # with open('./Data/RELEVAMIENTO DE AULAS - AULA.csv') as csv_file:
 #     csv_reader = csv.reader(csv_file, delimiter=',')
